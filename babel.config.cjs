8dda903e8f261cd1e57388b74726028de383110b
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2024-04-21 01:38:57 +0200

    chore: Remove postinstall script, add module resolver to fix tests (#82)
    ## Description
    
    Fixes deps installation errors and failing tests

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,3 +1,16 @@
 module.exports = {
+  "plugins": [
+    [
+      'module-resolver',
+      {
+        alias: {
+          // This needs to be mirrored in tsconfig.json
+          'eslint-config-react-native-matipl01': './index.js'
+        },
+        extensions: ['.ts', '.js'],
+        root: ['./']
+      }
+    ]
+  ],
   "presets": ["@babel/preset-env"]
 }
