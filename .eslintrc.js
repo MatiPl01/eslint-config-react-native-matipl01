b22688e9e833ee769dc109f1c3af3f2cfed0811f
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2024-04-20 15:03:53 +0200

    Start updating eslint config to the new eslint API

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+import eslintConfigPrettier from "eslint-config-prettier";
+import perfectionistNatural from 'eslint-plugin-perfectionist/configs/recommended-natural'
+
+export default [
+  perfectionistNatural,
+  eslintConfigPrettier
+]
