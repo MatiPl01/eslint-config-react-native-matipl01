ec98af690633f808f7e80aa94e605d0c23afe98b
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2024-04-21 01:36:51 +0200

    Add module resolver to fix tests

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,3 +1,16 @@
 module.exports = {
-  "presets": ["@babel/preset-env"]
+  "presets": ["@babel/preset-env"],
+  "plugins": [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        extensions: ['.ts', '.js'],
+        alias: {
+          // This needs to be mirrored in tsconfig.json
+          'eslint-config-react-native-matipl01': './index.js'
+        }
+      }
+    ]
+  ]
 }
