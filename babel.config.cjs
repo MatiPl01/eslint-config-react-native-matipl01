f3d465873f6b469427e3cacebbe6f2eb79404ecb
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2024-04-21 01:38:03 +0200

    Fix linting issue

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,16 +1,16 @@
 module.exports = {
-  "presets": ["@babel/preset-env"],
   "plugins": [
     [
       'module-resolver',
       {
-        root: ['./'],
-        extensions: ['.ts', '.js'],
         alias: {
           // This needs to be mirrored in tsconfig.json
           'eslint-config-react-native-matipl01': './index.js'
-        }
+        },
+        extensions: ['.ts', '.js'],
+        root: ['./']
       }
     ]
-  ]
+  ],
+  "presets": ["@babel/preset-env"]
 }
