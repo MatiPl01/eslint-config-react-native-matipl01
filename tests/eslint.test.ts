c33dbfcbcf694e8e2d150da3c7a4f17f556bce17
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-06-13 16:23:47 +0200

    Remove unnecessary comment

--- tests/eslint.test.ts
+++ tests/eslint.test.ts
@@ -1,13 +1,11 @@
 import { ESLint } from 'eslint';
 
-
 describe('eslint-config', () => {
   const eslint = new ESLint({
     overrideConfigFile: 'index.js',
   });
 
   it('still merges to the same rule set', async () => {
-    // 3⃣  Await the ESLint promise *inside* expect
     const cfg = await eslint.calculateConfigForFile('dummy.tsx');
     expect(cfg).toMatchSnapshot();
   });
